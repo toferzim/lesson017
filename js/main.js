e9e3a3926c76ef14eed5800d5b5bb0821a0a06c1
Anton <anton.zimin@gmail.com>
2020-06-15 17:58:46 +0300

    Первый комит

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let btn = document.getElementById("start"),
+    budgetValue = document.getElementsByClassName("budget-value"),
+    dayBudgetValue = document.getElementsByClassName("daybudget-value"),
+    expensesValue = document.getElementsByClassName("expenses-value"),
+    optionalexpensesValue = document.getElementsByClassName("optionalexpenses-value"),
+    incomeValue = document.getElementsByClassName("income-value"),
+    monthsavingsValue = document.getElementsByClassName("monthsavings-value"),
+    yearsavingsValue = document.getElementsByClassName("yearsavings-value"),
+    expensesItems = document.getElementsByClassName("expenses-item"),
+    expensesItemBtn = document.getElementsByClassName("oexpenses-item-btn"),
+    optionalExpensesBtn = document.getElementsByClassName("optionalexpenses"),
+    countBudgetBtn = document.getElementsByClassName("count-budget-btn"),
+    optionalExpensesItem = document.querySelectorAll(".optionalexpenses-item"),
+    chooseIncome = document.querySelector(".choose-income"),
+    savings = document.querySelector(".savings"),
+    chooseSum = document.querySelector(".choose-sum"),
+    choosePercent = document.querySelector(".choose-percent"),
+    yearValue = document.querySelector(".year-value"),
+    monthValue = document.querySelector(".month-value"),
+    dayValue = document.querySelector(".day-value");
+
+    
+
+
+
+console.log(optionalExpensesItem);
+// console.log(budgetValue);
+// console.log(dayBudgetValue);
+// console.log(expensesValue);
+// console.log(optionalexpensesValue);
+// console.log(incomeValue);
+// console.log(monthsavingsValue);
+// console.log(yearsavingsValue);
+// console.log(monthValue);
+// console.log(dayValue);
+
